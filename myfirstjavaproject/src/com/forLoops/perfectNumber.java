19c15dd74cbf04b95bb10be5eecf3b1b7548d318
Jayachandra <chinnu123488@gmail.com>
2026-09-18 09:37:27 +0530

    program to find the Perfect Number using For Loop !!

---
+++ myfirstjavaproject/src/com/forLoops/perfectNumber.java
@@ -0,0 +1,37 @@
+package com.forLoops;
+
+//program to find the Perfect Number using For Loop !!
+
+import java.util.Scanner;
+
+public class perfectNumber {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int n =sc.nextInt();
+		boolean status =isPerfect(n);
+
+	if(status) {
+		System.out.println("The number is perfect !!");
+	} else {
+		System.out.println("The number is not perfect !!");
+	}
+	sc.close();
+	}
+	
+	   static boolean isPerfect(int n) {
+		boolean status =false;
+		int sum =0;
+        for(int i=1;i<=100;i++) {
+			if (n%i == 0) {
+				sum =sum +i;
+			}
+		}
+			if(sum == n){
+				status = true;
+				}
+			return status;
+   }
+
+}
