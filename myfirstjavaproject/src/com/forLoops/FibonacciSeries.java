47fb39972bd9c7b37670f39076bf0ff3cdca240d
Jayachandra <chinnu123488@gmail.com>
2026-09-20 20:22:09 +0530

    Fibonacci series !!

---
+++ myfirstjavaproject/src/com/forLoops/FibonacciSeries.java
@@ -0,0 +1,31 @@
+package com.forLoops;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+
+	public static void main(String[] args) {
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter a number :");
+		int n = sc.nextInt();
+		fibonacciSeries(n);
+		
+		sc.close();
+
+	}
+	
+	static void fibonacciSeries(int n) {
+		int n1=0;
+		int n2=1;
+		int n3=0;
+		System.out.print(n1 +" " + n2 + " ");
+		
+		for(int i=1;i<=n-2;i++) {
+			n3=n1 + n2;
+			System.out.print(n3 +" ");
+			n1 =n2;
+			n2 =n3;
+		}
+	}
+
+}
