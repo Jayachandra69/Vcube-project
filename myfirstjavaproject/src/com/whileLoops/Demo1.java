a26842b1136f46d6a7e07ac3a8ae5e1c6b11618c
Jayachandra <chinnu123488@gmail.com>
2026-09-21 22:12:48 +0530

    Understanding basic while loop !!

---
+++ myfirstjavaproject/src/com/whileLoops/Demo1.java
@@ -0,0 +1,17 @@
+package com.whileLoops;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Main method started !!");
+		
+		int a =10;
+		while(a >=0) {
+			System.out.println("Welcome :" + a);
+			a--;
+		}
+		
+        System.out.println("Main method Ended !!");
+	}
+
+}
